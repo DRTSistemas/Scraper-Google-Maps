d26bb8ae6e9e8c7d30f9affab2bce931128f2f98
felipemonteiro44 <40672943+felipemonteiro44@users.noreply.github.com>
2024-09-10 15:01:58 -0300

    Fix: ReactQuery refetchOnWindowFocus

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,16 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
